9d0d359ab862f35c02187ec87ddbec8a9b34b0fc
jesus <jgr.dev.mw>
2019-01-03 18:59:38 +0100

    c0 - Mensaje bienvenida

---
+++ mmcalculadora/AppCalculadora.java
@@ -0,0 +1,14 @@
+package mmcalculadora;
+
+public class AppCalculadora {
+	
+	public void menu() {
+		System.out.println("CALCULADORA");
+	}
+
+	public static void main(String[] args) {
+		AppCalculadora calcu=new AppCalculadora();
+		calcu.menu();
+	}
+
+}
