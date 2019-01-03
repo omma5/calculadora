6b5b1e42067077547f82d99070e779d4a1042263
jesus <jgr.dev.mw>
2019-01-03 19:19:17 +0100

    ya sale

--- mmcalculadora/AppCalculadora.java
+++ mmcalculadora/AppCalculadora.java
@@ -1,14 +1,25 @@
 package mmcalculadora;
 
+import java.util.Scanner;
+
 public class AppCalculadora {
 	
 	public void menu() {
 		System.out.println("CALCULADORA");
+		System.out.println("Para salir pulse 0");
 	}
 
 	public static void main(String[] args) {
-		AppCalculadora calcu=new AppCalculadora();
-		calcu.menu();
+		AppCalculadora appcalcu=new AppCalculadora();
+		boolean continuar=true;
+		Scanner sc =new Scanner(System.in);
+		char opcion='0';
+		while(continuar) {
+			appcalcu.menu();
+			opcion=sc.next().charAt(0);
+			if(opcion=='0') continuar=false;
+		}
+		System.out.println("Adios");
 	}
 
 }
