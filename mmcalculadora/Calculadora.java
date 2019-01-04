0eb5ecb91845ad7ee8a28a8a8efc15424d5067ae
jesus <jdamdaw@gmail.com>
2019-01-04 00:11:44 +0000

    creo clase Calculadora con sus atributos, consultores, modificadores y toString

---
+++ mmcalculadora/Calculadora.java
@@ -0,0 +1,31 @@
+package mmcalculadora;
+
+public class Calculadora {
+	//atributos
+	private double num1;
+	private double num2;
+	//constructor con parámetros
+	public Calculadora(double num1, double num2) {
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+	//consultores
+	public double getNum1() {
+		return num1;
+	}
+	public double getNum2() {
+		return num2;
+	}
+	//modificadores
+	public void setNum1(double num1) {
+		this.num1 = num1;
+	}
+	public void setNum2(double num2) {
+		this.num2 = num2;
+	}
+	//toString
+	@Override
+	public String toString() {
+		return "Calculadora [num1=" + num1 + ", num2=" + num2 + "]";
+	}
+}
